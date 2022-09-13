841c155f82436e588d52c5e3b40e0457b1d8f167
gerroldk <gerroldk@yahoo.com>
2022-09-13 22:49:54 +0100

    Different app server

--- app/app.py
+++ app/app.py
@@ -1,4 +1,9 @@
-from flask import Flask
+#!/usr/bin/python3
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+import json
+from socketserver import ThreadingMixIn
+import threading
 
 print("Reading config")
 
@@ -11,18 +16,33 @@ except Exception as e:
     print(e.__class__)
     raise e
 
-app = Flask(__name__)
+hostName = "0.0.0.0"
+serverPort = 5000
 
 
-@app.route("/")
-def hello_world():
-    return "Hello, Docker!"
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/config":
+            self.send_response(200)
+            self.send_header("Content - type", "text / html")
+            self.end_headers()
+            content = open("/config/config.txt", 'rb').read()
+            self.wfile.write(content)
+        else:
+            self.send_response(404)
+        return
 
 
-@app.route("/config")
-def output_config():
-    return config
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    """Handle requests in a separate thread."""
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    webServer = ThreadedHTTPServer((hostName, serverPort), Handler)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        passwebServer.server_close()
+        print("Server stopped.")
